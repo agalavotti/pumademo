0d8bc79e14ae3b14f56029ab40faa1ba166d406f
agalavotti <64584397+agalavotti@users.noreply.github.com>
2022-05-13 14:52:26 +0200

    Delete webpush-service-worker.js

--- webpush-service-worker.js
+++
@@ -1,3 +0,0 @@
-importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
-var config = '{"appserviceKey":"BHkUaqF-mJxYs9oSkR8TYmyfOnbDZ24wiNEloFXGSbyzQ3zz7-IRVfatceJRza5BB6W6VaM7r9UL8_2zIAc7guU=","apiKey":"ABERJTz-FMgOAht4aNQPmHn60","accountToken":"ABEjXW4zOVJYL6soMj1REOLrE","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
-orawp.init(config);
